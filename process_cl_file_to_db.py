b6ba5fad291f5ecdbcee581d6643ed6442d3f196
Dmytro Vakulenko <vakulenko.dmitry@gmail.com>
2024-10-19 11:41:02 +0200

    another intermediate

--- process_cl_file_to_db.py
+++ process_cl_file_to_db.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 import os
 import re
 
-#==================================================================================
-# 0. Input
-#==================================================================================
+#############################################
+### 0. Input
+#############################################
 
 repo_path = "C:/Users/vakul/source/repos/googletest/"
 file_to_process = repo_path + "googletest/src/gtest-all.cc"
@@ -32,9 +32,9 @@ def parse_input(include_folders, defines):
 
     return args
 
-#==================================================================================
-# 1. Initial setup
-#==================================================================================
+#############################################
+### 1. Initial setup
+#############################################
 
 # Load environment variables
 load_dotenv()
@@ -48,16 +48,16 @@ gremlin_client = client.Client('wss://example-opencv.gremlin.cosmos.azure.com:44
                                password=os.getenv("COSMOS_DB_PRIMARY_KEY"),
                                message_serializer=serializer.GraphSONSerializersV2d0())
 
-#==================================================================================
-# 2. Gremlin-related methods
-#==================================================================================
+#############################################
+### 2. Gremlin-related methods
+#############################################
 
 # Will make relative file path and will remove unsupported symbols
 def get_file_id(file_path, replacement_symbol = '_'):
     if file_path.startswith(repo_path):
         relative_path = os.path.relpath(file_path, repo_path)
     else:
-        relative_path = file_path.replace("C:/Program Files/", "system/")
+        relative_path = os.path.basename(file_path)
     
     adjusted_path = relative_path.replace('\\', replacement_symbol).replace('/', replacement_symbol)
     return adjusted_path
@@ -68,39 +68,47 @@ def get_id(cursor, replacement_symbol = '_'):
     if id:
         id = id.replace('#', replacement_symbol)
         if not cursor.is_definition():
-            id = id + cursor.location.file.name + cursor.location.line
+            if cursor == cursor.canonical:
+                id = f"{id}"
+            else:
+                id = f"{id}@{get_file_id(cursor.location.file.name)}@{cursor.location.line}"
     if not id:
         raise ValueError("ID cannot be empty!")
     return id
 
-def vertex_exists(cursor):
+def vertex_exists(cursor):    
     query = "g.V().has('id', id).count()"
     bindings = {"id": get_id(cursor)}
-    
+    return False
     # Execute the query and get the result
-    result = gremlin_client.submit(query, bindings).all().result()
+    #result = gremlin_client.submit(query, bindings).all().result()
     
     # Return True if the count is greater than 0, meaning the vertex exists
-    return result[0] > 0
+    #return result[0] > 0
 
 tu_cached = None
 
 # Add vertex to Gremlin database
-def add_vertex_to_gremlin(label, cursor, properties = {}):
+def add_vertex_to_gremlin(cursor, properties = {}):
+    global tu_cached
+
     properties["id"] = get_id(cursor)
+    properties["label"] = cursor.kind.name
+    properties["usr"] = cursor.get_usr()
+    properties["spelling"] = cursor.spelling
     if tu_cached is None:
         tu_cached = get_file_id(cursor.translation_unit.spelling)
     properties["tu"] = tu_cached
 
     properties_pairs = [(key, value) for key, value in properties.items()]
     query = "g.addV(label)"
-    bindings = {"label": label}
+    bindings = {}
 
     for i, (key, value) in enumerate(properties_pairs):
         query += f".property('{key}', key_{i})"
         bindings[f"key_{i}"] = value
 
-    gremlin_client.submit(query, bindings)
+    #gremlin_client.submit(query, bindings)
 
 # Add edge to Gremlin database
 def add_edge_to_gremlin(from_cursor, edge_label, to_cursor, property_key=None, property_value=None):
@@ -122,45 +130,216 @@ def add_edge_to_gremlin(from_cursor, edge_label, to_cursor, property_key=None, p
     # Finalize the query
     query += ".from('from')"
 
-    gremlin_client.submit(query, bindings)
+    #gremlin_client.submit(query, bindings)
 
-#==================================================================================
-# 3. Adding properties for specific types
-#==================================================================================
+#############################################
+### 3. Adding properties for specific types
+#############################################
 
-#==================================================================================
-# 4. Processing
-#==================================================================================
+def add_is_static_storage_property(cursor, properties):
+    properties['is_static'] = cursor.storage_class == clang.cindex.StorageClass.STATIC
+
+def add_is_exported_property(cursor, properties):
+    properties['is_exported'] = cursor.linkage == clang.cindex.LinkageKind.EXTERNAL
+
+def add_access_specifier(cursor, properties):
+    access_specifier_map = {
+        clang.cindex.AccessSpecifier.PUBLIC: "public",
+        clang.cindex.AccessSpecifier.PRIVATE: "private",
+        clang.cindex.AccessSpecifier.PROTECTED: "protected",
+        clang.cindex.AccessSpecifier.INVALID: "none"
+    }
+    properties['access_specifier'] = access_specifier_map.get(cursor.access_specifier, "none")
+
+def get_field_properties(cursor, properties):
+    properties = {}
+    add_is_exported_property(cursor, properties)
+    add_is_static_storage_property(cursor, properties)
+    add_access_specifier(cursor, properties)
+    return properties
+
+def get_member_function_properties(cursor):
+    properties = {
+        'is_const': cursor.is_const_method(),
+        'is_static': cursor.is_static_method(),
+        'is_virtual': cursor.is_virtual_method(),
+        'is_pure_virtual': cursor.is_pure_virtual_method(),
+        'is_defaulted': cursor.is_default_method(),
+        'is_deleted': cursor.is_deleted_method()
+    }
+    add_is_exported_property(cursor, properties)
+    add_access_specifier(cursor, properties)
+    return properties
+
+def get_constructor_properties(cursor):
+    properties = {
+        'is_copy_constructor': cursor.is_copy_constructor(),
+        'is_default_constructor': cursor.is_default_constructor(),
+        'is_move_constructor': cursor.is_move_constructor(),
+        'is_converting_constructor': cursor.is_converting_constructor()
+    }
+    add_access_specifier(cursor, properties)
+    return properties
+
+def get_destructor_properties(cursor):
+    properties = {
+        'is_virtual': cursor.is_virtual_method(),
+        'is_pure_virtual': cursor.is_pure_virtual_method(),
+        'is_deleted': cursor.is_deleted_method(),
+        'is_defaulted': cursor.is_default_method(),
+    }
+    add_is_exported_property(cursor, properties)
+    add_access_specifier(cursor, properties)
+    return properties
+
+def get_conversion_function_properties(cursor):
+    properties = {
+        'is_explicit': cursor.is_explicit_method(),
+        'is_virtual': cursor.is_virtual_method(),
+        'is_pure_virtual': cursor.is_pure_virtual_method(),
+        'is_defaulted': cursor.is_default_method(),
+        'is_deleted': cursor.is_deleted_method(),
+    }
+    add_is_exported_property(cursor, properties)
+    add_access_specifier(cursor, properties)
+    return properties
+
+#############################################
+### 4. Processing
+#############################################
 
 # Function to process cursor based on its kind
-def process_cursor(cursor):
-    try:
-        if cursor.is_definition():
-            if cursor.kind == clang.cindex.CursorKind.FUNCTION_DECL:
-                print(f"Found a function declaration: {cursor.spelling}")
-            elif cursor.kind == clang.cindex.CursorKind.CLASS_DECL:
-                print(f"Found a class declaration: {cursor.spelling}")
-            elif cursor.kind == clang.cindex.CursorKind.VAR_DECL:
-                print(f"Found a variable declaration: {cursor.spelling}")
-            # Add more cases as needed for other cursor types
-        # Example: Process cursor references
-        if cursor.kind == clang.cindex.CursorKind.TYPE_REF:
-            referenced_cursor = cursor.referenced
-            print(f"Type reference: {referenced_cursor.spelling}")
-    except Exception as e:
-        print(f"Error processing cursor at {cursor.location.line}: {e}")
+def process_definition_cursor_as_vertex(cursor):
+    properties = {}
+
+    if cursor.kind.is_invalid():
+        raise ValueError('Invalid kind of cursor')
+    if cursor.kind.is_unexposed():
+        raise ValueError('Unexposed kind of cursor')
+
+    if cursor.kind.is_declaration():
+        if (cursor.kind is CursorKind.STRUCT_DECL or 
+            cursor.kind is CursorKind.UNION_DECL or
+            cursor.kind is CursorKind.CLASS_DECL or
+            cursor.kind is CursorKind.ENUM_DECL):
+            add_is_exported_property(cursor, properties)
+        elif cursor.kind is CursorKind.FIELD_DECL:
+            properties = get_field_properties(cursor, properties)
+        elif cursor.kind is CursorKind.ENUM_CONSTANT_DECL:
+            pass
+        elif cursor.kind is CursorKind.FUNCTION_DECL:
+            add_is_exported_property(cursor, properties)
+        elif cursor.kind is CursorKind.VAR_DECL:
+            add_is_static_storage_property(cursor, properties)
+        elif cursor.kind is CursorKind.PARM_DECL:
+            pass
+        elif cursor.kind is CursorKind.TYPEDEF_DECL:
+            pass
+        elif cursor.kind is CursorKind.CXX_METHOD:
+            properties = get_member_function_properties(cursor)
+        elif cursor.kind is CursorKind.NAMESPACE:
+            pass
+        elif cursor.kind is CursorKind.LINKAGE_SPEC:
+            return
+        elif cursor.kind is CursorKind.CONSTRUCTOR:
+            properties = get_constructor_properties(cursor)
+        elif cursor.kind is CursorKind.DESTRUCTOR:
+            properties = get_destructor_properties(cursor)
+        elif cursor.kind is CursorKind.CONVERSION_FUNCTION:
+            properties = get_conversion_function_properties(cursor)
+        elif (cursor.kind is CursorKind.TEMPLATE_TYPE_PARAMETER or
+            cursor.kind is CursorKind.TEMPLATE_NON_TYPE_PARAMETER or
+            cursor.kind is CursorKind.TEMPLATE_TEMPLATE_PARAMETER or
+            cursor.kind is CursorKind.FUNCTION_TEMPLATE or
+            cursor.kind is CursorKind.CLASS_TEMPLATE or
+            cursor.kind is CursorKind.CLASS_TEMPLATE_PARTIAL_SPECIALIZATION):
+            pass
+        elif (cursor.kind is CursorKind.NAMESPACE_ALIAS or
+            cursor.kind is CursorKind.USING_DIRECTIVE or
+            cursor.kind is CursorKind.USING_DECLARATION or
+            cursor.kind is CursorKind.TYPE_ALIAS_DECL):
+            pass
+        else:
+            return
+
+        add_vertex_to_gremlin(cursor, properties)
+    elif cursor.kind.is_reference():
+        pass
+    elif cursor.kind.is_expression():
+        pass
+    elif cursor.kind.is_statement():
+        pass
+
+def process_definition_cursor_edges(cursor):
+    if cursor.kind.is_invalid():
+        raise ValueError('Invalid kind of cursor')
+    if cursor.kind.is_unexposed():
+        raise ValueError('Unexposed kind of cursor')
+
+    if cursor.kind.is_declaration():
+        if (cursor.kind is CursorKind.CXX_METHOD or
+            cursor.kind is CursorKind.CONSTRUCTOR or
+            cursor.kind is CursorKind.DESTRUCTOR or
+            cursor.kind is CursorKind.CONVERSION_FUNCTION):
+            add_edge_to_gremlin(cursor.semantic_parent, 'contains_method', cursor.get_definition())
+        elif cursor.kind is CursorKind.FIELD_DECL:
+            add_edge_to_gremlin(cursor.semantic_parent, 'contains_field', cursor)
+        elif cursor.kind is CursorKind.PARM_DECL:
+            add_edge_to_gremlin(cursor.semantic_parent, 'contains_argument', cursor)
+        elif cursor.kind is CursorKind.ENUM_CONSTANT_DECL:
+            add_edge_to_gremlin(cursor.semantic_parent, 'contains_value', cursor)
+        #elif cursor.kind is CursorKind.TYPEDEF_DECL:
+            #tp = cursor.underlying_typedef_type.get_declaration()            
+    elif cursor.kind.is_reference():
+        if cursor.kind == CursorKind.CXX_BASE_SPECIFIER:
+            add_edge_to_gremlin(cursor.semantic_parent, 'inherits', cursor.referenced)
+    elif cursor.kind.is_expression():
+        pass
+    elif cursor.kind.is_statement():
+        pass
+
+# Process translation unit 
+def process_translation_unit(tu):
+    # create nodes
+    for cursor in tu.cursor.walk_preorder():
+        try:
+            if cursor.kind.is_translation_unit():
+                pass
+            elif cursor.is_definition():
+                process_definition_cursor_as_vertex(cursor)
+            else:
+                if cursor.kind.is_declaration():
+                    add_vertex_to_gremlin(cursor)
+        except Exception as e:
+            print(f"ERROR processing cursor at {cursor.location.file.name}:{cursor.location.line}: {e}")
+    
+    # create edges
+    for cursor in tu.cursor.walk_preorder():
+        try:
+            if cursor.kind.is_translation_unit():
+                pass
+            elif cursor.is_definition():
+                process_definition_cursor_edges(cursor)
+            else:
+                if cursor.kind.is_declaration():
+                    add_edge_to_gremlin(cursor, 'declares', cursor.canonical)
+        except Exception as e:
+            print(f"ERROR processing cursor at {cursor.location.file.name}:{cursor.location.line}: {e}")
+
+#############################################
+### 5. Main
+#############################################
 
 # Create translation unit
 index = clang.cindex.Index.create()
 clang_args = parse_input(include_folders, defines)
 translation_unit = index.parse(file_to_process, args=[
-    '-x', 'c++',            # Specify C++ language
-    '-std=c++11',           # Set the C++ standard (adjust if needed)
-    '-fparse-all-comments', # Optional flags
+    '-x', 'c++',
+    '-std=c++11', 
+    '-fparse-all-comments',
     '-fno-delayed-template-parsing',
     '-ferror-limit=0'
 ] + clang_args)
 
 # Walk the entire translation unit in preorder
-for cursor in translation_unit.cursor.walk_preorder():
-    process_cursor(cursor)
+process_translation_unit(translation_unit)
